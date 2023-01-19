e27cbea468627ab1c4b411a57ca403a3cc346b50
Julie-licite <66413145+Julie-licite@users.noreply.github.com>
2023-01-19 17:08:47 +0100

    GoodZone, fix script destruction d'objet

--- Assets/ScriptANous/StoreFacade.cs
+++ Assets/ScriptANous/StoreFacade.cs
@@ -13,55 +13,101 @@ public class StoreFacade : MonoBehaviour
     bool gotFacadeRiche;
     bool gotFacadeBoulanger;
     bool gotFacadeGarde;
+    bool jeSuisDansUneFacade;
 
+    public GameObject facadeActuelleToDestroy;
+    string facadeActuelle;
 
-
-
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
-        //si j'ai touché la facade riche, je peux appuyer sur E pour "pondre" le prefab facade riche, ça détruit l'objet touché aussi 
+        
+
+
+       
+
+        //si j'ai touche la facade riche, je peux appuyer sur E pour dÃ©truire l'object touche
         //manque zone appuyable que dans la zone de l'objet pour le "manger"
-        //manque coordonée pour l'objet instantié.
-        if (gotFacadeRiche == true ) 
+        //manque coordonee pour l'objet instantie.
+        if (jeSuisDansUneFacade == true)
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (gotFacadeRiche == true && Input.GetKeyDown(KeyCode.E))
             {
-                Instantiate(objetFacadeRiche);
+                //Instantiate(objetFacadeRiche);
                 gotFacadeRiche = false;
-                DestroyImmediate(objetFacadeRiche, true);
+                Destroy(facadeActuelleToDestroy);
+                
+
             }
-            
         }
+        
     }
 
     private void OnTriggerEnter(Collider collision)
     {
-        //je recup ma variable d'un autre script et je lui donne un nouveau nom
-        string facadeActuelle = GameObject.Find("Cube").GetComponent<facadeRamassable>().nomFacade;
-
-        print(facadeActuelle);
-
-        // je passe ma variable "j'ai l'objet" en true
-        if (facadeActuelle == "facadeRiche")
+        if (collision.gameObject.GetComponent<facadeRamassable>() != null)
         {
-            gotFacadeRiche = true;
-        }
+            
+            jeSuisDansUneFacade = true;
 
-        else if (facadeActuelle == "facadeGarde")
+            //je recup ma variable d'un autre script et je lui donne un nouveau nom
+            facadeActuelle = collision.gameObject.GetComponent<facadeRamassable>().nomFacade;
+            facadeActuelleToDestroy = collision.gameObject;
+
+            print(facadeActuelle);
+
+            // quand je rentre dans la zone de la facade,je passe ma variable "j'ai l'objet" en true
+            if (facadeActuelle == "facadeRiche")
+            {
+                gotFacadeRiche = true;
+            }
+
+            else if (facadeActuelle == "facadeGarde")
+            {
+                gotFacadeGarde = true;
+            }
+
+            else if (facadeActuelle == "facadeBoulanger")
+            {
+                gotFacadeBoulanger = true;
+            }
+         }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.GetComponent<facadeRamassable>() != null)
         {
 
-        }
+        
+            //je lui dis de recup la var facade actuelle sur le scirpt du cube
 
-        else if (facadeActuelle == "facadeBoulanger")
-        {
+            facadeActuelleToDestroy = null;
 
+            print(facadeActuelle);
+
+            jeSuisDansUneFacade = false;
+
+            // quand je rentre dans la zone de la facade,je passe ma variable "j'ai l'objet" en true
+            if (facadeActuelle == "facadeRiche")
+            {
+                gotFacadeRiche = false;
+            }
+
+            else if (facadeActuelle == "facadeGarde")
+            {
+                gotFacadeGarde = false;
+            }
+
+            else if (facadeActuelle == "facadeBoulanger")
+            {
+                gotFacadeBoulanger = false;
+            }
         }
     }
 }
